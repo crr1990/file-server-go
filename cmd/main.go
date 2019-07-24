8e4c6d66b609b15a41e560c1d6eaa75d04ffac50
eric.chen <eric.chen@dadaabc.com>
2019-07-24 09:51:57 +0800

    上传文件

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"log"
+	"os"
+	"io"
+)
+
+type  ReturnData struct{
+	Code int
+	Message string
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	http.HandleFunc("/upload", uploader)
+
+
+	err := http.ListenAndServe(":9090",nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func uploader(w http.ResponseWriter, r *http.Request)  {
+
+	//判断请求方式
+	if r.Method == "POST" {
+		//设置内存大小
+		r.ParseMultipartForm(32 << 20);
+		//获取上传的第一个文件
+		file, header, err := r.FormFile("file");
+		defer file.Close();
+		if err != nil {
+			log.Fatal(err);
+		}
+		//创建上传目录
+		os.Mkdir("./upload", os.ModePerm);
+		//创建上传文件
+		cur, err := os.Create("./upload/" + header.Filename);
+		defer cur.Close();
+		if err != nil {
+			log.Fatal(err);
+		}
+		//把上传文件数据拷贝到我们新建的文件
+		io.Copy(cur, file);
+	}
+
 }
